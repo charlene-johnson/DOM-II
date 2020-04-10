9439058a7e782415acd8bbff74b886b35bd7dc7a
charlene-johnson <charlene.j8234@gmail.com>
2020-04-09 22:32:43 -0400

    Created a few events on the DOM for the pictures

--- js/index.js
+++ js/index.js
@@ -1,1 +1,26 @@
 // Your code goes here
+const funBus = document.querySelector(".intro img");
+funBus.addEventListener("mouseenter", () => {
+    funBus.style.transform = "scale(1.5)";
+    funBus.style.transition = "transform 1s";
+})
+funBus.addEventListener("mouseleave", () => {
+    funBus.style.transform = "scale(1)";
+})
+
+
+const letsGoPic = document.querySelector(".img-content img")
+letsGoPic.addEventListener("click", () => {
+    letsGoPic.src = "img/ocean.jpg"
+})
+letsGoPic.addEventListener("dblclick", () => {
+    letsGoPic.src = "img/adventure.jpg"
+})
+
+const allPics = document.querySelectorAll("img")
+allPics.forEach((pics) => {
+    pics.addEventListener("mouseover", () => {
+        pics.style.transform = "rotate(360deg)";
+        pics.style.transition = "transform 1s";
+    })
+})
