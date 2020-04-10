4cadebff538725b9166f905fa6bb00dffd6675f6
charlene-johnson <charlene.j8234@gmail.com>
2020-04-10 00:57:18 -0400

    Finished 4 events, need 6 more (one is commented out and will be working on that soon)

--- js/index.js
+++ js/index.js
@@ -9,18 +9,54 @@ funBus.addEventListener("mouseleave", () => {
 })
 
 
-const letsGoPic = document.querySelector(".img-content img")
-letsGoPic.addEventListener("click", () => {
-    letsGoPic.src = "img/ocean.jpg"
+const middlePics = document.querySelectorAll(".img-content img")
+middlePics[0].addEventListener("click", () => {
+    middlePics[0].src = "img/maps.jpg"
 })
-letsGoPic.addEventListener("dblclick", () => {
-    letsGoPic.src = "img/adventure.jpg"
+middlePics[0].addEventListener("dblclick", () => {
+    middlePics[0].src = "img/adventure.jpg"
+})
+middlePics[1].addEventListener("click", () => {
+    middlePics[1].src = "img/ocean.jpg"
+})
+middlePics[1].addEventListener("dblclick", () => {
+    middlePics[1].src = "img/fun.jpg"
 })
 
-const allPics = document.querySelectorAll("img")
+const destinationPic = document.querySelector(".content-destination img")
+destinationPic.addEventListener("click", () => {
+    destinationPic.src = "img/boats.jpg"
+})
+destinationPic.addEventListener("dblclick", () => {
+    destinationPic.src = "img/destination.jpg"
+})
+
+const allPics = document.querySelectorAll(".img-content, .content-destination img")
 allPics.forEach((pics) => {
-    pics.addEventListener("mouseover", () => {
-        pics.style.transform = "rotate(360deg)";
-        pics.style.transition = "transform 1s";
+    pics.addEventListener("mouseenter", () => {
+        var isAnimating = false;
+        
+        pics.onmouseenter = function(){
+            if (isAnimating) {
+                return;
+            }
+            isAnimating = true;
+            pics.style.transform = "rotate(360deg)";
+            pics.style.transition = "transform 1s";
+
+            setTimeout(function(){
+                pics.style.transform = "rotate(0deg)";
+                pics.style.transition = "";
+                isAnimating = false;
+            }, 1000);
+        }
+        
     })
 })
+
+// const allBackground = document.querySelector("body")
+
+// allBackground.addEventListener("wheel", () => {
+//     allBackground.style.backgroundColor = "limegreen"
+// })
+
